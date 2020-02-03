ef134e9d3e86418ae2928633111b986de70cffd5
ruthw <rwustenberg@gmail.com>
2020-02-02 20:58:45 -0600

    styling updated

--- tipCalc.js
+++ tipCalc.js
@@ -1,32 +1,33 @@
 //calc tip
 const calcTip = () => {
-    let billAmnt = document.getElementById("billAmount").value
-    let qualService = document.getElementById("qualService").value
-    let numOfDiners = document.getElementById("diners").value
+    let billAmnt = 
+document.getElementById("billAmount").value
+    let qualService = 
+document.getElementById("qualService").value
+    let numOfDiners = 
+document.getElementById("diners").value
+    
     //checks for missed fields
     if (billAmnt === "" || qualService === 0) {
-       return alert("Please Enter Values")
-       console.log("hello")
+        console.log("No Values Entered in Form")
+        return alert("Please Enter Values")
     }
     // Check to see if input is empty or 1 or less
     if (numOfDiners === "" || numOfDiners <= 1 ) {
         numOfDiners = 1
         document.getElementById("each").style.display = "none"
     } 
-        else { document.getElementById("each").style.display = "block"
-        }
+    else {
+        document.getElementById("each").style.display = "block"
+    }
+
     // Calculate Tip
     let total = (billAmnt * qualService) / numOfDiners
-        total = Math.round(total * 100)
     //Ensures 2 places always after decimal
-        total = total.toFixed(2)
+    total = Math.round(total).toFixed(2)
+    console.log("the Total is: " + total)
+
     //Display tip
-        document.getElementById("totalTip").style.display = "block"
-        document.getElementById("tip").innerHTML = total
-    //Hide the tip amount on load
-        document.getElementById("totalTip").style.display = "none"
-        document.getElementById("each").style.display = "none"
-    // on click to display
-        document.getElementById("calculate").onclick = calcTip()
-    }
-    console.log(calcTip())
+    document.getElementById("totalTip").style.display = "block"
+    document.getElementById("tip").innerHTML = total
+}
